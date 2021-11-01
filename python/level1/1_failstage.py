d5fc52d5873aee1dc495ffa22725ba0c01fddfeb
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2021-11-02 01:03:53 +0900

    edit failstage

--- python/level1/1_failstage.py
+++ python/level1/1_failstage.py
@@ -1,40 +1,23 @@
 #실패율
 #https://programmers.co.kr/learn/courses/30/lessons/42889
+import numpy as np
 def solution(N, stages):
     answer = []
-    count = [0] * N
     stop = [0] * N
+    np_count = np.array([0] * N)
 
     for j in stages:
-        for i in range(1, N + 1):
-            if (i >= j):
-                count[i - 1] += 1
-                stop[i - 1] += 1
-                break
-            else:
-                count[i - 1] += 1
-    # print(count, stop)
+        np_count[:j] += 1
+    for i in range(1, N + 1):
+        stop[i - 1] = stages.count(i)
+    print(np_count, stop)
 
-    fail = []
-    for i in range(len(count)):
-        if (count[i] == 0):
-            fail.append(0)
+    fail = {}
+    for i in range(len(np_count)):
+        if (np_count[i] == 0):
+            fail[i + 1] = 0
         else:
-            fail.append(stop[i] / count[i])
+            fail[i + 1] = stop[i] / np_count[i]
+    print(fail)
 
-    new_dict = dict(zip(range(len(fail)), fail))
-    # print(new_dict)
-
-    rank = sorted(new_dict.items(), key=lambda item: item[1], reverse=True)
-
-    # print(rank)
-
-    for i in range(len(rank)):
-        answer.append(rank[i][0] + 1)
-
-    # for i in range(len(fail)):
-    #     max_index = fail.index(max(fail))
-    #     answer.append(max_index+1)
-    #     fail[max_index] = -1
-
-    return answer
+    return sorted(fail, key=lambda x: fail[x], reverse=True)
