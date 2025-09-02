81d8f446a0f2b74329853e21e2f5431fbfc9e8c5
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2025-09-02 16:36:12 +0900

    fix) 오픈채팅방

--- java/level2/2_chatroom.java
+++ java/level2/2_chatroom.java
@@ -1,47 +1,77 @@
-//¿ÀÇÂÃ¤ÆÃ¹æ
+//ï¿½ï¿½ï¿½ï¿½Ã¤ï¿½Ã¹ï¿½
 //https://programmers.co.kr/learn/courses/30/lessons/42888?language=java
 import java.util.*;
+
 class Solution {
+
+
+    private String _doAction(String[] userRecord, Map<String,String> users) {
+
+        switch(userRecord[0]) {
+            case "Change":
+                break;
+            case "Enter":
+                return users.get(userRecord[1]) + "ë‹˜ì´ ë“¤ì–´ì™”ìŠµë‹ˆë‹¤.";
+            case "Leave":
+                return users.get(userRecord[1]) + "ë‹˜ì´ ë‚˜ê°”ìŠµë‹ˆë‹¤.";
+            default:
+                break;
+        }
+
+        return "";
+    }
+
+
+    private Map<String, String> _createUsers(String[] record) {
+        Map<String, String> users = new HashMap<String,String>();
+
+        for(String r : record) {
+            // 1. ë“¤ì–´ì˜¨ ì •ë³´ split
+            String[] userRecord = r.split(" ");
+
+            // 2. Change, Enterì¸ ê²½ìš° id, name ì €ì¥
+            switch(userRecord[0]) {
+                case "Change":
+                case "Enter":
+                    users.put(userRecord[1], userRecord[2]);
+                    break;
+                case "Leave":
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // System.out.println(users);
+
+        return users;
+
+    }
+
     public String[] solution(String[] record) {
-        
-        Map<String, String> id_hash = new HashMap();
-        ArrayList<ArrayList<String>> msg = new ArrayList<>();
-        for (int i=0; i<record.length; i++) {
-            ArrayList<String> temp = new ArrayList<>();
-            String[] str_list = record[i].split(" ");
-            // for (String str : str_list) {
-            //     System.out.print(str);
-            // }
-            // System.out.println();
-            if (str_list[0].equals("Enter")) {
-                id_hash.put(str_list[1], str_list[2]);
-                temp.add(str_list[0]);
-                temp.add(str_list[1]);
-                msg.add(temp);
-            }
-            else if(str_list[0].equals("Change")) {
-                id_hash.put(str_list[1], str_list[2]);
-            }
-            else {
-                temp.add(str_list[0]);
-                temp.add(str_list[1]);
-                msg.add(temp);
+
+
+        // 1. ìœ ì € ì •ë³´ ìƒì„±
+        Map<String, String> users = _createUsers(record);
+
+        // 2. ë™ì‘ ì‹¤í–‰
+        ArrayList<String> answerList = new ArrayList<>();
+
+        for (String r: record) {
+            String[] userRecord = r.split(" ");
+            String actionString = _doAction(userRecord, users);
+
+            if(actionString.length() > 0) {
+                answerList.add(_doAction(userRecord, users));
             }
         }
-        // for (Map.Entry<String, String> entry : id_hash.entrySet()) {
-        //     System.out.println(entry.getKey() + " : " + entry.getValue());
-        // }
-        
-        String[] answer = new String[msg.size()];
-        for (int i =0; i<msg.size(); i++) {
-            if (msg.get(i).get(0).equals("Enter")) {
-                answer[i] = id_hash.get(msg.get(i).get(1))+"´ÔÀÌ µé¾î¿Ô½À´Ï´Ù.";
-            }
-            else {
-                answer[i] = id_hash.get(msg.get(i).get(1))+"´ÔÀÌ ³ª°¬½À´Ï´Ù.";
-            }
+
+        // 3. Array ë³€í™˜
+        String[] answer = new String[answerList.size()];
+        for(int i =0; i<answerList.size(); i++) {
+            answer[i] = answerList.get(i);
         }
+
         return answer;
-        
     }
 }
