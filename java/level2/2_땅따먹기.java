eed46a2c5d5944d562fb08eb1ea8989448ba80b5
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2025-09-02 15:10:37 +0900

    fix) 땅따먹기

--- java/level2/2_땅따먹기.java
+++ java/level2/2_땅따먹기.java
@@ -1,11 +1,12 @@
 // dfs -> 시간초과
-
+import java.util.*;
 class Solution {
     static int max = 0;
 
     int solution(int[][] land) {
 
 
+        /* dfs 풀이
         for (int i =0; i< 4; i++) {
             _dfs(land,0,i, 0);
         }
@@ -13,6 +14,23 @@ class Solution {
 
         int answer = 0;
         return max;
+         */
+
+        for (int i =1; i<land.length; i++) {
+            land[i][0] = land[i][0] + Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] = land[i][1] + Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] = land[i][2] + Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] = land[i][3] + Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+        }
+
+
+        int answer = 0;
+        for (int i=0; i<4; i++) {
+            if(land[land.length-1][i] > answer) {
+                answer = land[land.length-1][i];
+            }
+        }
+        return answer;
 
     }
 
